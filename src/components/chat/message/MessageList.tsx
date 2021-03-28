e303230ca70834ae8cf56ffc307a763f6275ef96
Kuruyia <alexandresollier@gmail.com>
2021-03-28 16:14:48 +0200

    MessageList: Added autoscroll to the bottom of the list

--- src/components/chat/message/MessageList.tsx
+++ src/components/chat/message/MessageList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {MessageData} from '../../../api/message/MessageData';
 import MessageItem from './MessageItem';
 import './MessageList.scss';
@@ -37,8 +37,53 @@ interface MessageListProps {
  * @constructor
  */
 const MessageList: React.FunctionComponent<MessageListProps> = (props: MessageListProps) => {
+    // Component refs
+    let divRef = useRef<HTMLDivElement | null>(null);
+    let autoScroll = useRef<boolean>(true);
+
+    // Set autoscroll depending on if the list is scrolled all the way down
+    const onMessageListScroll = (e: React.UIEvent<HTMLDivElement>) => {
+        if (divRef.current !== null) {
+            autoScroll.current = (divRef.current?.scrollTop + divRef.current?.clientHeight + 16 >=
+                divRef.current?.scrollHeight);
+        }
+    };
+
+    // Scrolls to the bottom of the message list
+    const scrollToBottom = () => {
+        if (divRef.current !== null) {
+            divRef.current?.scroll({
+                top: divRef.current?.scrollHeight,
+                behavior: 'auto',
+            });
+        }
+    };
+
+    // Scroll to the bottom when data changed and autoscroll is enabled
+    useEffect(() => {
+        if (autoScroll.current) {
+            scrollToBottom();
+        }
+    }, [props.data]);
+
+    // Scroll to the bottom when window size changed and autoscroll is enabled
+    useEffect(() => {
+        const onWindowResize = (e: UIEvent) => {
+            if (autoScroll.current) {
+                scrollToBottom();
+            }
+        };
+
+        window.addEventListener('resize', onWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', onWindowResize);
+        };
+    }, []);
+
     return (
-        <div className={'message-list'}>
+        <div className={'message-list'} ref={(ref) => divRef.current = ref}
+             onScroll={onMessageListScroll}>
             {props.data.map((elem, idx, array) => {
                 // If this is the first message or not the same sender as the previous message,
                 // show the username and send date
